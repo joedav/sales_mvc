b1817b45d730e360b0d0603b0a0cba3dc12427ec
joedav <joelyson.david@live.com>
2020-10-22 22:36:36 -0300

    CRUD scaffolding

---
+++ Sales_MVC/Data/Sales_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sales_MVC.Models;
+
+namespace Sales_MVC.Data
+{
+    public class Sales_MVCContext : DbContext
+    {
+        public Sales_MVCContext (DbContextOptions<Sales_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sales_MVC.Models.Department> Department { get; set; }
+    }
+}
--- Sales_MVC/Startup.cs
+++ Sales_MVC/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Sales_MVC.Data;
 
 namespace Sales_MVC
 {
@@ -24,6 +26,9 @@ namespace Sales_MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<Sales_MVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Sales_MVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
