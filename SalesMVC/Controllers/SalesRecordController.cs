611c379777560407fa08b84f8f93330338d7bbb3
joedav <joelyson.david@live.com>
2020-10-31 21:13:33 -0300

    Preparing sales navigations views

---
+++ SalesMVC/Controllers/SalesRecordController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesMVC.Controllers
+{
+    /// <summary>
+    /// Sales record controller
+    /// </summary>
+    public class SalesRecordController : Controller
+    {
+        /// <summary>
+        /// View index of sales record
+        /// </summary>
+        /// <returns>Sales record</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
